766a00eaf5175a6f9460f2277668b77d2eb563f7
Maxim Mulintsev <n30fly@gmail.com>
2018-03-09 17:16:05 +0600

    update version to 1.2.1

--- lib/Resque.php
+++ lib/Resque.php
@@ -11,7 +11,7 @@ require_once dirname(__FILE__) . '/Resque/Exception.php';
  */
 class Resque
 {
-	const VERSION = '1.2';
+	const VERSION = '1.2.1';
 
 	/**
 	 * @var Resque_Redis Instance of Resque_Redis that talks to redis.
